65a0f12fc1eb0832dfa858e9140a9017fabce190
Kaweees <49963287+Kaweees@users.noreply.github.com>
2020-06-30 22:20:49 -0700

    Update thermometer.ino

--- thermometer.ino
+++ thermometer.ino
@@ -6,23 +6,22 @@
 // Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
-const int thermisistorPin = A0; // thermisistorPin
+const int thermisistorPin = A0; // thermisistor model: TMP36
 const int redLED = 11; // red LED
 const int greenLED = 12; // green LED
 const int yellowLED = 13; // yellow LED
-int count = 0;
 
+int count = 0;
 void setup() {
-  // Initiate the LCD:
-  lcd.init();
+  lcd.init(); // Initiate the LCD
   lcd.backlight();
-  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
-  lcd.print("Hello World!"); // Print the string "Hello World!"
 }
 void loop() {
   // Print 'Hello World!' on the first line of the LCD:
-  lcd.setCursor(0, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
   count ++;
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row
   lcd.print("Temp: " + String(count) + char(223) + "C" );
+  lcd.setCursor(0, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("Voltage: " + String(count) + " V" );
   delay(500);
 }
