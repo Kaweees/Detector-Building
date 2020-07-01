7c0e8ee508831ccfc74d8a190ae45bc951164a61
Kaweees <49963287+Kaweees@users.noreply.github.com>
2020-06-30 20:11:32 -0700

    Supports i2c covered lcd display
    LCD

--- thermometer.ino
+++ thermometer.ino
@@ -1,39 +1,30 @@
-const int redLED = 3;
-const int greenLED = 4;
-const int yellowLED = 5;
+/* I2C LCD with Arduino example code. More info: https://www.makerguides.com */
+// Include the libraries:
+// LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C 
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h> // Library for LCD
+// Wiring: SDA pin is connected to A4 and SCL pin to A5.
+// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+const int thermisistorPin = A0; // thermisistorPin
+const int redLED = 11; // red LED
+const int greenLED = 12; // green LED
+const int yellowLED = 13; // yellow LED
+
+
+int count = 0;
 void setup() {
-  // put your setup code here, to run once:
-  // initialize the digital pin as an output.
-  pinMode(redLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-
+  // Initiate the LCD:
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("Hello World!"); // Print the string "Hello World!"
 }
-
 void loop() {
-  int temp = Serial.read();
-  Serial.print("Number received: ");
-  Serial.println(temp);
-  // put your main code here, to run repeatedly:
-  if ((20 >= temp) && (temp >= 10)){
-    digitalWrite(greenLED, HIGH);     // turn the LED on (HIGH is the voltage level)
-    delay(1000); // basically time.sleep(miliseconds)
-    digitalWrite(greenLED, LOW);      // turn the LED off by making the voltage LOW
-    }
-  else if ((40 >= temp) && (temp > 20)){
-    digitalWrite(yellowLED, HIGH);     // turn the LED on (HIGH is the voltage level)
-    delay(1000); // basically time.sleep(miliseconds)
-    digitalWrite(yellowLED, LOW);      // turn the LED off by making the voltage LOW
-    }
-  else if ((60 >= temp) && (temp > 40)){
-    digitalWrite(redLED, HIGH);     // turn the LED on (HIGH is the voltage level)
-    delay(1000); // basically time.sleep(miliseconds)
-    digitalWrite(redLED, LOW);      // turn the LED off by making the voltage LOW
-    }
-  else{
-    digitalWrite(greenLED, HIGH);     // turn the LED on (HIGH is the voltage level)
-    digitalWrite(yellowLED, HIGH);     // turn the LED on (HIGH is the voltage level)
-    digitalWrite(redLED, HIGH);     // turn the LED on (HIGH is the voltage level)
-  }
-
+  // Print 'Hello World!' on the first line of the LCD:
+  lcd.setCursor(0, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  count ++;
+  lcd.print("Temp: " + String(count) + char(223) + "C" );
+  delay(500);
 }
